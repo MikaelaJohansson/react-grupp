9e4e7d4084ba730204ac1399ee9faf09121d6ed9
Mikaela Johansson <mikaela.johansson87@gmail.com>
2024-03-24 15:43:34 +0100

    formulär och POST klar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
-import './App.css'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import ActionForm from './ActionForm';
+import './App.css';
 
 function App() {
-  
-
   return (
-    <>
-    <p>mikaela</p>
-    </>
-  )
+    <BrowserRouter>
+      <div>
+        <NavLink to="/ActionForm">ActionForm</NavLink>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/ActionForm" element={<ActionForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
